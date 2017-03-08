5d0a483cc68e2b6ca7360a7aed78231534e9138a
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-03-08 19:53:18 +0300

    Add args4j library; Add short parsing stuff

--- src/main/java/parser/Main.java
+++ src/main/java/parser/Main.java
@@ -1,45 +1,46 @@
 package parser;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.diff.DiffEntry;
-import org.eclipse.jgit.diff.DiffFormatter;
-import org.eclipse.jgit.revwalk.RevCommit;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.OptionDef;
+import org.kohsuke.args4j.spi.OneArgumentOptionHandler;
+import org.kohsuke.args4j.spi.Setter;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * @author @muratovv
  * @date 20.02.17
  */
 public class Main {
-    public static void main(String[] args) throws GitAPIException, IOException {
-        System.out.println("Hello world");
-        Git                 git     = Git.open(new File("test_repo"));
-        Iterable<RevCommit> commits = git.log().call();
-        for (RevCommit commit : commits) {
-            System.out.println(new Committed(commit));
+    public static void main(String[] args) {
+        InputParameters p      = new InputParameters();
+        CmdLineParser   parser = new CmdLineParser(p);
+        System.out.println("target repo: " + p.targetRepository);
+    }
+
+    private static class InputParameters {
+        @Option(name = "target", required = true, handler = RepoHelper.class)
+        private File targetRepository;
+    }
+
+    /**
+     * Helper class for parsing repository
+     */
+    private static class RepoHelper extends OneArgumentOptionHandler<File> {
+
+        public RepoHelper(CmdLineParser parser, OptionDef option, Setter<? super File> setter) {
+            super(parser, option, setter);
         }
 
-        ByteArrayOutputStream out       = new ByteArrayOutputStream();
-        DiffFormatter         formatter = new DiffFormatter(out);
-        formatter.setRepository(git.getRepository());
-        Iterator<RevCommit> history = git.log().call().iterator();
-        RevCommit           new_commit    = history.next();
-        RevCommit           old_commit    = history.next();
-
-        formatter.format(old_commit, new_commit);
-        List<DiffEntry>     entries = formatter.scan(old_commit, new_commit);
-        formatter.flush();
-        System.out.println(out.toString());
-        for (DiffEntry entry : entries) {
-            
-
+        @Override
+        protected File parse(String argument) throws NumberFormatException, CmdLineException {
+            File f = new File(argument);
+            if (!f.isDirectory()) new CmdLineException(owner, "Repository must be folder");
+            if (f.list((dir, name) -> ".git".equals(name)).length == 0)
+                new CmdLineException(super.owner, "Folder must contains .git folder");
+            return f;
         }
-
     }
 }
