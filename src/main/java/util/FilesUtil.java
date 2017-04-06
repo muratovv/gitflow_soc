842d044cf664496ca5e872f7574ec04c4c784596
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-06 10:05:50 +0300

    Fix exception when file not exists

--- src/main/java/util/FilesUtil.java
+++ src/main/java/util/FilesUtil.java
@@ -1,5 +1,9 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
@@ -9,6 +13,7 @@ import java.nio.file.Path;
  * @date 05.04.17
  */
 public class FilesUtil {
+    private static final Logger logger = LoggerFactory.getLogger(FilesUtil.class);
 
     /**
      * Write data to file
@@ -20,11 +25,17 @@ public class FilesUtil {
      */
     public static boolean writeFile(String path, String data) {
         try {
+            File file = new File(path);
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
             PrintWriter writer = new PrintWriter(path, "UTF-8");
             writer.write(data);
             writer.close();
             return true;
         } catch (IOException e) {
+            logger.error("writeFile", e);
             return false;
         }
     }
