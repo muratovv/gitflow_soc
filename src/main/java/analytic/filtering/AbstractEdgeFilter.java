56115f53e20cda44234392d68cadb391732842ba
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-07 19:04:48 +0300

    remove useless methods

--- src/main/java/analytic/filtering/AbstractEdgeFilter.java
+++ src/main/java/analytic/filtering/AbstractEdgeFilter.java
@@ -2,8 +2,6 @@ package analytic.filtering;
 
 import analytic.graphs.AuthorEdge;
 import org.eclipse.collections.api.set.ImmutableSet;
-import org.eclipse.collections.api.set.MutableSet;
-import org.eclipse.collections.impl.factory.Sets;
 
 /**
  * @author @muratovv
@@ -28,36 +26,4 @@ public abstract class AbstractEdgeFilter {
     }
 
     protected abstract boolean filter(AuthorEdge edge);
-
-    public static AbstractEdgeFilter combineAndFilters(ImmutableSet<AuthorEdge> targetCollection,
-                                                       AbstractEdgeFilter... filters) {
-        MutableSet<AuthorEdge> newEdges = Sets.mutable.empty();
-        for (AuthorEdge edge : targetCollection) {
-            boolean getEdge = true;
-            for (AbstractEdgeFilter filter : filters) {
-                if (!filter.filter(edge)) {
-                    getEdge = false;
-                    break;
-                }
-            }
-            if (getEdge) newEdges.add(edge);
-        }
-        return new DefaultEdgeFilter(newEdges.toImmutable());
-    }
-
-    public static AbstractEdgeFilter combineOrFilters(ImmutableSet<AuthorEdge> targetCollection,
-                                                      AbstractEdgeFilter... filters) {
-        MutableSet<AuthorEdge> newEdges = Sets.mutable.empty();
-        for (AuthorEdge edge : targetCollection) {
-            boolean getEdge = false;
-            for (AbstractEdgeFilter filter : filters) {
-                if (filter.filter(edge)) {
-                    getEdge = true;
-                    break;
-                }
-            }
-            if (getEdge) newEdges.add(edge);
-        }
-        return new DefaultEdgeFilter(newEdges.toImmutable());
-    }
 }
