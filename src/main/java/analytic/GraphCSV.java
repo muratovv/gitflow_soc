51f9634c7871237a67a2d56789d5d2853eb1587d
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-07 14:19:05 +0300

    Reformat class GraphCSV; Add Edge subclass; Making more clear interface

--- src/main/java/analytic/GraphCSV.java
+++ src/main/java/analytic/GraphCSV.java
@@ -1,7 +1,12 @@
 package analytic;
 
 import git.Author;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.impl.factory.Lists;
 import util.Strings;
+
+import java.util.Locale;
 
 /**
  * @author @muratovv
@@ -17,24 +22,37 @@ public class GraphCSV {
         this.analytic = analytic;
     }
 
-    public String makeCosineCSV() {
+    public String makeCosineBlobCSV() {
         StringBuilder builder = new StringBuilder();
-        builder
-                .append(generateMetaInformation())
+        builder.append(generateMetaInformation())
                 .append(makeHead());
-        int size = analytic.getAuthors().size();
+        getEdges().forEach(edge -> {
+            String format = generateCSVrow(edge);
+            builder.append(format);
+        });
+        return builder.toString();
+    }
+
+    private static String generateCSVrow(Edge<Author, Author, Double> edge) {
+        String doubleInStrRepr = String.format(Locale.GERMAN, "%f", edge.tag);
+        return String.format("%s%s %s%s %s\n",
+                generateName(edge.source), separator, generateName(edge.dest), separator, doubleInStrRepr);
+    }
+
+    public ImmutableList<Edge<Author, Author, Double>> getEdges() {
+        MutableList<Edge<Author, Author, Double>> edges = Lists.mutable.empty();
+        int                                       size  = analytic.getAuthors().size();
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 Author a             = analytic.getAuthors().get(i);
                 Author b             = analytic.getAuthors().get(j);
                 double cosineMeasure = analytic.cosine(a, b);
                 if (filter.apply(cosineMeasure)) {
-                    builder.append(String.format("%s%s %s%s %s\n",
-                            generateName(a), separator, generateName(b), separator, cosineMeasure));
+                    edges.add(Edge.construct(a, b, cosineMeasure));
                 }
             }
         }
-        return builder.toString();
+        return edges.toImmutable();
     }
 
     private static String generateName(Author author) {
@@ -60,5 +78,40 @@ public class GraphCSV {
 
     public interface Filter {
         boolean apply(Double value);
+    }
+
+    /**
+     * Data class for edge
+     *
+     * @param <S> source vertex
+     * @param <D> dest vertex
+     * @param <T> weight (or tag) of edge
+     */
+    public static class Edge<S, D, T> {
+        S source;
+        D dest;
+        T tag;
+
+        public S getSource() {
+            return source;
+        }
+
+        public D getDest() {
+            return dest;
+        }
+
+        public T getTag() {
+            return tag;
+        }
+
+        public Edge(S source, D dest, T tag) {
+            this.source = source;
+            this.dest = dest;
+            this.tag = tag;
+        }
+
+        public static <S, D, T> Edge<S, D, T> construct(S source, D dest, T tag) {
+            return new Edge<>(source, dest, tag);
+        }
     }
 }
