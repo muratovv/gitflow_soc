73fa35110940582995ed1e86398cf1162da59deb
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-11 18:08:03 +0300

    Fix types on tests

--- src/test/java/analytic/AbstractGraphFilterTest.java
+++ src/test/java/analytic/AbstractGraphFilterTest.java
@@ -4,8 +4,8 @@ import analytic.filtering.SpanningTreeFilter;
 import analytic.graphs.AuthorEdge;
 import analytic.graphs.Edge;
 import git.Author;
-import org.eclipse.collections.api.list.ImmutableList;
-import org.eclipse.collections.impl.factory.Lists;
+import org.eclipse.collections.api.set.ImmutableSet;
+import org.eclipse.collections.impl.factory.Sets;
 import org.junit.Test;
 
 import static analytic.graphs.AuthorEdge.make;
@@ -18,16 +18,16 @@ import static org.junit.Assert.*;
 public class AbstractGraphFilterTest {
     @Test
     public void spanningTreeTest() throws Exception {
-        Author                    V1     = Author.make("a", "a");
-        Author                    V2     = Author.make("b", "b");
-        Author                    V3     = Author.make("c", "c");
-        Author                    V4     = Author.make("d", "d");
-        AuthorEdge                E12    = make(V1, V2, 1.);
-        AuthorEdge                E23    = make(V2, V3, 2.);
-        AuthorEdge                E34    = make(V3, V4, 3.);
-        AuthorEdge                E41    = make(V1, V4, 4.);
-        ImmutableList<AuthorEdge> edges  = Lists.immutable.of(E12, E23, E34, E41);
-        SpanningTreeFilter        filter = new SpanningTreeFilter(edges.toSet().toImmutable());
+        Author                   V1     = Author.make("a", "a");
+        Author                   V2     = Author.make("b", "b");
+        Author                   V3     = Author.make("c", "c");
+        Author                   V4     = Author.make("d", "d");
+        AuthorEdge               E12    = make(V1, V2, 1.);
+        AuthorEdge               E23    = make(V2, V3, 2.);
+        AuthorEdge               E34    = make(V3, V4, 3.);
+        AuthorEdge               E41    = make(V1, V4, 4.);
+        ImmutableSet<AuthorEdge> edges  = Sets.immutable.of(E12, E23, E34, E41);
+        SpanningTreeFilter       filter = new SpanningTreeFilter(edges);
 
 
         assertFalse(filter.inSpanningTree(E41));
