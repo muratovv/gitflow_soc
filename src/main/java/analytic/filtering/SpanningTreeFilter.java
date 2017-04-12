1b2bb17df942a5dff6169436dc7069b040842ae4
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-04-12 11:21:45 +0300

    Fix spanning tree. Now it returns MAXIMUM spanning tree

--- src/main/java/analytic/filtering/SpanningTreeFilter.java
+++ src/main/java/analytic/filtering/SpanningTreeFilter.java
@@ -1,17 +1,23 @@
 package analytic.filtering;
 
 import analytic.graphs.AuthorEdge;
-import git.Author;
 import org.eclipse.collections.api.set.ImmutableSet;
+import org.jgrapht.Graph;
 import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm;
-import org.jgrapht.alg.spanning.KruskalMinimumSpanningTree;
+import org.jgrapht.alg.util.UnionFind;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 /**
  * @author @muratovv
  * @date 11.04.17
  */
 public class SpanningTreeFilter extends AbstractGraphFilter {
-    private SpanningTreeAlgorithm.SpanningTree<AuthorEdge> spanningTree = null;
+    private static final Logger                                         logger       = LoggerFactory
+            .getLogger(SpanningTreeFilter.class);
+    private              SpanningTreeAlgorithm.SpanningTree<AuthorEdge> spanningTree = null;
 
     public SpanningTreeFilter(ImmutableSet<AuthorEdge> edges) {
         super(edges);
@@ -19,8 +25,9 @@ public class SpanningTreeFilter extends AbstractGraphFilter {
     }
 
     private SpanningTreeAlgorithm.SpanningTree<AuthorEdge> makeSpanningTree() {
-        KruskalMinimumSpanningTree<Author, AuthorEdge> treeWrapper = new KruskalMinimumSpanningTree<>(graph);
-        return treeWrapper.getSpanningTree();
+        SpanningTreeAlgorithm.SpanningTree<AuthorEdge> spanningTree = new MaxSpanningTree<>(graph).getSpanningTree();
+        logger.info("Spanning tree weight {}", spanningTree.getWeight());
+        return spanningTree;
     }
 
     public boolean inSpanningTree(AuthorEdge edge) {
@@ -30,6 +37,40 @@ public class SpanningTreeFilter extends AbstractGraphFilter {
     @Override
     public boolean filter(AuthorEdge edge) {
         return super.filter(edge) && inSpanningTree(edge);
+    }
+
+    private static class MaxSpanningTree<V, E> implements SpanningTreeAlgorithm<E> {
+
+        private final Graph<V, E> graph;
+
+        public MaxSpanningTree(Graph<V, E> graph) {
+
+            this.graph = graph;
+        }
+
+        @Override
+        public SpanningTreeAlgorithm.SpanningTree<E> getSpanningTree() {
+            UnionFind<V> forest   = new UnionFind<>(this.graph.vertexSet());
+            ArrayList<E> allEdges = new ArrayList<>(this.graph.edgeSet());
+            allEdges.sort(Comparator.comparing(edge2 -> Double.valueOf(this.graph.getEdgeWeight(edge2))));
+            Collections.reverse(allEdges);
+            double   spanningTreeCost = 0.0D;
+            Set<E>   edgeList         = new HashSet();
+            Iterator var6             = allEdges.iterator();
+
+            while (var6.hasNext()) {
+                E edge   = ((E) var6.next());
+                V source = this.graph.getEdgeSource(edge);
+                V target = this.graph.getEdgeTarget(edge);
+                if (!forest.find(source).equals(forest.find(target))) {
+                    forest.union(source, target);
+                    edgeList.add(edge);
+                    spanningTreeCost += this.graph.getEdgeWeight(edge);
+                }
+            }
+
+            return new SpanningTreeAlgorithm.SpanningTreeImpl(edgeList, spanningTreeCost);
+        }
     }
 
 }
